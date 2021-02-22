ba76db7bd143903d96958508cef3c7c0bcc834e0
Ahmed Elmahdy <43157710+mahdy-world@users.noreply.github.com>
2021-02-22 21:35:16 +0200

    add new job

--- job/views.py
+++ job/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import job
 from django.template import Context
 from django.core.paginator import Paginator
-from .form import ApplyForm
+from .form import ApplyForm ,JobForm
 
 # Create your views here.
 
@@ -10,7 +10,6 @@ def job_list(request):
     all_jobs_list = job.objects.all()
 
     paginator = Paginator(all_jobs_list, 1) # Show 25 contacts per page.
-
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -34,5 +33,15 @@ def job_detail(request, slug):
 
     context = {'job' : job_detail , 'form' : ApplyForm}
     return render(request,'job/job_detail.html',context)
+
+
+def add_job(request):
+
+    if request.method=='POST':
+        pass 
+    else:
+        form = JobForm()    
+    
+    return render(request,'job/add_job.html',{'form':form})
 
 
